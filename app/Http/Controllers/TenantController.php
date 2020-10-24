57d62705dd7bfea9bd574dbed6e2b333a90858ff
daniloCorvus <danilo.grisalez@corvuslab.co>
2020-10-24 12:45:21 -0500

    sabado24

--- app/Http/Controllers/TenantController.php
+++ app/Http/Controllers/TenantController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Admin;
 use App\Models\Tenant;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-
-use function GuzzleHttp\Promise\all;
+use App\Helpers\DatabaseConnection;
+use Illuminate\Support\Facades\Config;
 
 class TenantController extends Controller
 {
@@ -17,7 +18,44 @@ class TenantController extends Controller
      */
     public function index()
     {
-        return response()->json(['data' =>Tenant::all()]);
+
+        // DB::reconnect('mysql');
+        // $admin = new Admin();
+        // $admin->setConnection('mysql');
+        // $admin1 = $admin->find(1);
+        // return response()->json(['data' =>   $admin1, 'conection' => Config::get("database.connections.mysql"), 'cnt' => DB::connection()->getDatabaseName()]);
+
+        Config::set("database.connections.mysql.database", 'corvus');
+        DB::reconnect('mysql');
+        $admin = new Admin();
+        $admin->setConnection('mysql');
+        $admin1 = $admin->find(1);
+        return response()->json(DB::connection()->getDatabaseName());
+
+        // return response()->json(['data' =>   $admin1, 'conection' => Config::get("database.connections.mysql"), 'cnt' => DB::connection()->getDatabaseName()]);
+
+        // DB::purge('Tenantcy');
+        // Config::set("database.connections.mysql.database", 'corvus');
+        // DB::reconnect('mysql');
+
+
+        // Config::set('database.connections.mysql', array(
+        //     'driver'    => 'mysql',
+        //     'host'      => 'localhost',
+        //     'database'  => 'corvus',
+        //     'username'  => 'local',
+        //     'password'  => '123',
+        //     'charset'   => 'utf8',
+        //     'collation' => 'utf8_general_ci',
+        //     'prefix'    => '',
+        // ));
+
+        // Config::get('database.default');
+        // return DB::getDatabaseName();
+        // $admin = new Admin();
+        // $admin->setConnection('mysql');
+        // $admin1 = $admin->find(1);
+        // return $admin1;
     }
 
     /**
